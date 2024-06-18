9e8eb7b900f659e46411dfdb8a1da5d2c51ead81
Ram22an <118821139+Ram22an@users.noreply.github.com>
2024-06-18 13:14:15 +0530

    Adding player movement

---
+++ Assets/Script/PlayerScript/PlayerMove.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Rigidbody2D MyBody;
+    private float Move_ForceX = 2f, Move_ForceY = 2f;
+    void Awake()
+    {
+        MyBody = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        Move();
+    }
+    void Move()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        /**if (moveX != 0)
+        {
+            Vector3 localScale = transform.localScale;
+            localScale.x = moveX > 0 ? -Mathf.Abs(localScale.x) : Mathf.Abs(localScale.x);
+            transform.localScale = localScale;
+        }**/
+        if (moveX >0)
+        {
+            gameObject.transform.localScale = new Vector2(-1f, 1f);
+        }
+        if(moveX<0)
+        {
+            gameObject.transform.localScale = new Vector2(1f, 1f);
+        }
+
+        Vector2 movement = new Vector2(moveX * Move_ForceX, moveY* Move_ForceY);
+            MyBody.MovePosition(MyBody.position + movement * Time.fixedDeltaTime);
+    }
+}
