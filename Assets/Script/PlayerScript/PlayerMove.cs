a35684890684e6cd67623ff242e2adf55e27edc8
Ram22an <118821139+Ram22an@users.noreply.github.com>
2024-08-23 00:17:06 +0530

    updated player move

--- Assets/Script/PlayerScript/PlayerMove.cs
+++ Assets/Script/PlayerScript/PlayerMove.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    private PlayerAnimation playerAnimation;
     private Rigidbody2D MyBody;
     private float Move_ForceX = 2f, Move_ForceY = 2f;
     void Awake()
     {
+        playerAnimation = GetComponent<PlayerAnimation>();
         MyBody = GetComponent<Rigidbody2D>();
     }
     void FixedUpdate()
@@ -32,8 +34,17 @@ public class PlayerMove : MonoBehaviour
         {
             gameObject.transform.localScale = new Vector2(1f, 1f);
         }
+        if (moveY != 0 || moveX != 0)
+        {
+            playerAnimation.PlayerWalking(true);
+        }
+        else
+        {
+            playerAnimation.PlayerWalking(false);
+        }
 
         Vector2 movement = new Vector2(moveX * Move_ForceX, moveY* Move_ForceY);
             MyBody.MovePosition(MyBody.position + movement * Time.fixedDeltaTime);
     }
-}
+
+}//class
